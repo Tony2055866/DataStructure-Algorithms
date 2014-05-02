c097cbd243fa8fb7390ebfea0354b1e570e46b7b
gaotong2055 <gaotong2055@gmail.com>
2014-05-02 18:05:58 +0800

    LeetCode-Single Number II[位运算]

---
+++ SingleNumber2/src/SingleNumber2.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : SingleNumber2.cpp
+// Author      : Gaotong
+// Version     :
+// Copyright   : www.acmerblog.com
+// Description : leetCode, Single Number II.
+// Your algorithm should have a linear runtime complexity.
+// Could you implement it without using extra memory?
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	//直接的实现就是用大小为 32的数组来记录所有 位上的和。
+    int singleNumber(int A[], int n) {
+    	int ans;
+
+    	return 0;
+    }
+
+    int singleNumberA(int A[], int n) {
+
+
+    }
+};
+
+int main() {
+
+	return 0;
+}
